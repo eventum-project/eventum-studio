e5f0c04de405c7d6ec74f3eec20007d7587f50ad
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-30 13:39:34 +0300

    Update theme applying

--- eventum_studio/__main__.py
+++ eventum_studio/__main__.py
@@ -6,11 +6,18 @@ import streamlit.web.cli as st_cli
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 STUDIO_ENTRYPOINT = os.path.join(BASE_PATH, '01_Time_distribution.py')
 
+THEME_OPTIONS = [
+    '--theme.primaryColor=#8282ef',
+    '--theme.backgroundColor=#181818',
+    '--theme.secondaryBackgroundColor=#252526',
+    '--theme.textColor=#e3e3e3',
+]
+
 
 def main() -> None:
     """Execute command to run streamlit app."""
     os.chdir(BASE_PATH)
-    sys.argv = ['streamlit', 'run', STUDIO_ENTRYPOINT]
+    sys.argv = ['streamlit', 'run', STUDIO_ENTRYPOINT, *THEME_OPTIONS]
     exit(st_cli.main())
 
 
