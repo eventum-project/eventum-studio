d193b75d42ac841ebc2a9abf5f00d357e268d849
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-05-30 12:51:21 +0300

    Update streamlit run command

--- eventum_studio/__main__.py
+++ eventum_studio/__main__.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+import streamlit.web.cli as st_cli
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 STUDIO_ENTRYPOINT = os.path.join(BASE_PATH, '01_Time_distribution.py')
@@ -7,7 +10,8 @@ STUDIO_ENTRYPOINT = os.path.join(BASE_PATH, '01_Time_distribution.py')
 def main() -> None:
     """Execute command to run streamlit app."""
     os.chdir(BASE_PATH)
-    os.system(f'streamlit run {STUDIO_ENTRYPOINT}')
+    sys.argv = ['streamlit', 'run', STUDIO_ENTRYPOINT]
+    exit(st_cli.main())
 
 
 if __name__ == '__main__':
