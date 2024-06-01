1e11b3a5f1aba555cc423a0431f6f0fa5c650958
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-06-01 11:13:13 +0300

    Add version argument

--- eventum_studio/__main__.py
+++ eventum_studio/__main__.py
@@ -1,8 +1,11 @@
+import argparse
 import os
 import sys
 
+import pkg_resources
 import streamlit.web.cli as st_cli
 
+VERSION = pkg_resources.get_distribution('eventum_studio').version
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 STUDIO_ENTRYPOINT = os.path.join(BASE_PATH, '01_Time_distribution.py')
 
@@ -16,6 +19,18 @@ THEME_OPTIONS = [
 
 def main() -> None:
     """Execute command to run streamlit app."""
+    argparser = argparse.ArgumentParser(
+        prog='eventum-studio',
+        description='Content designer for Eventum',
+        epilog='Documentation: https://eventum-generatives.github.io/Website/',
+    )
+    argparser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=f'eventum-studio {VERSION}'
+    )
+    argparser.parse_args()
+
     os.chdir(BASE_PATH)
     sys.argv = ['streamlit', 'run', STUDIO_ENTRYPOINT, *THEME_OPTIONS]
     exit(st_cli.main())
