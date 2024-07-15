6c087de069e0b507fc70e48f17e2129699a081ca
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2024-07-15 23:02:34 +0300

    Add primary colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,30 @@
+import flowbitePlugin from 'flowbite/plugin'
+
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#eff2fe',
+          100: '#e2e6fd',
+          200: '#cbd0fa',
+          300: '#abb1f6',
+          400: '#8282ef',
+          500: '#766de7',
+          600: '#6651da',
+          700: '#5742c0',
+          800: '#47389b',
+          900: '#3d347b',
+          950: '#251e48',
+      }
+      }
+    }
 	},
 
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [flowbitePlugin]
 } as Config;
